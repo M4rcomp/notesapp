3586cd26b8c8a262c4f10937aa8951301f984753
M4rcomp <marcoaurelio.login@gmail.com>
2025-01-26 02:02:10 -0300

    auth changes

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,4 +8,28 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  userAttributes: {
+    "custom:nome": {
+      dataType: "String",
+      mutable: true,
+      maxLen: 11,
+      minLen: 11,
+    },    
+    "custom:cpf": {
+      dataType: "String",
+      mutable: true,
+      maxLen: 11,
+      minLen: 11,
+    },
+    "custom:telefone": {
+      dataType: "Number",
+      mutable: true,
+      min: 1,
+      max: 100,
+    },
+    "custom:termos": {
+      dataType: "Boolean",
+      mutable: true,
+    },
+  },
 });
